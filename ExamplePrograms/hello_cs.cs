5cdfd371345e5c0224da3381c33e01ec386e86b8
ipv3 <57889136+ipv3@users.noreply.github.com>
2023-07-20 17:45:04 -0400

    Add files via upload

---
+++ ExamplePrograms/hello_cs.cs
@@ -0,0 +1,11 @@
+// a comment - hello_cs.cs
+// compile with: csc hello_cs.cs  --OR-- MS-Visual Studio
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main (string[] args)
+        {
+            System.Console.WriteLine ("Hello, World!");
+        }
+    }
+}
