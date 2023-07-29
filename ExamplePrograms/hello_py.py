c1eef24d82cd45e85848b5856062bb0d81d715cf
ipv3 <57889136+ipv3@users.noreply.github.com>
2023-07-29 17:01:38 -0400

    Update hello_py.py

--- ExamplePrograms/hello_py.py
+++ ExamplePrograms/hello_py.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3        (on Linux to specify the interpreter)
+#!/usr/bin/python3
+# (on Linux the previous line specifies the interpreter)
 # a comment - hello_py.py
 # To run on Linux:  ./hello_py.py
 # To run on Windows and Linux:  python3 hello_py.py
-
 print ("Hello, World!")
