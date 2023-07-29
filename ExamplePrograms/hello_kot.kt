ffd9512ff504f38958ca2c5c52e95d9478057dc1
ipv3 <57889136+ipv3@users.noreply.github.com>
2023-07-29 16:02:33 -0400

    Update hello_kot.kt

--- ExamplePrograms/hello_kot.kt
+++ ExamplePrograms/hello_kot.kt
@@ -1,5 +1,11 @@
-// helloworld.k
+// a comment  - hello_kt.kt     /* another comment */
+
+// Compile with: kotlinc hello_kt.kt
+//   this ends as <name>.kt  (must end in .kt)
+//   we compile to name: Hello_ktKt.class
+// run with: java Hello_ktKt
+//    this is H<name>Kt  (uppercase H, uppercase K, lowercase t)
+
 fun main () {
     println ("Hello, World!") ;
 }
-
